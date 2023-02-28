c2542394d51444018bee88685883f95379780689
iamzjt-front-end <iamzjt@foxmail.com>
2023-02-28 10:43:20 +0800

    :memo: refactor: 删除注释

--- reactivity/reactive.js
+++ reactivity/reactive.js
@@ -5,14 +5,12 @@ export function reactive(raw) {
 		get(target, key) {
 			const res = Reflect.get(target, key);
 
-			// TODO 依赖收集
 			track(target, key);
 			return res;
 		},
 		set(target, key, value) {
 			const res = Reflect.set(target, key, value);
 
-			// TODO 触发依赖
 			trigger(target, key);
 			return res;
 		}
